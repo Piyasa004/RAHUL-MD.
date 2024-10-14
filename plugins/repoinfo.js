373131e09568ef9dc6957788752129694797806c
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-15 00:26:31 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/repoinfo.js
+++ plugins/repoinfo.js
@@ -1,1 +1,28 @@
-tt
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+function _0x4678(_0x566b0b,_0xd01302){const _0x1f42b5=_0x1f42();return _0x4678=function(_0x467807,_0x4388b5){_0x467807=_0x467807-0x1c0;let _0x5303b3=_0x1f42b5[_0x467807];return _0x5303b3;},_0x4678(_0x566b0b,_0xd01302);}const _0x1539a9=_0x4678;(function(_0x2cf57b,_0x4ab356){const _0x101d3c=_0x4678,_0x189b7c=_0x2cf57b();while(!![]){try{const _0x1fa454=-parseInt(_0x101d3c(0x1d7))/0x1*(-parseInt(_0x101d3c(0x1cb))/0x2)+parseInt(_0x101d3c(0x1c9))/0x3*(parseInt(_0x101d3c(0x1cc))/0x4)+-parseInt(_0x101d3c(0x1d9))/0x5*(-parseInt(_0x101d3c(0x1db))/0x6)+-parseInt(_0x101d3c(0x1d5))/0x7+-parseInt(_0x101d3c(0x1c6))/0x8*(-parseInt(_0x101d3c(0x1ce))/0x9)+-parseInt(_0x101d3c(0x1d1))/0xa+parseInt(_0x101d3c(0x1cf))/0xb*(-parseInt(_0x101d3c(0x1c4))/0xc);if(_0x1fa454===_0x4ab356)break;else _0x189b7c['push'](_0x189b7c['shift']());}catch(_0x38c34c){_0x189b7c['push'](_0x189b7c['shift']());}}}(_0x1f42,0x6b4c5));const axios=require(_0x1539a9(0x1cd)),{cmd,commands}=require(_0x1539a9(0x1c0));cmd({'pattern':'srepo','desc':_0x1539a9(0x1c2),'category':'other','react':'🍃','filename':__filename},async(_0x5dde0d,_0xe0ba24,_0x4a08c7,{from:_0x207f7c,quoted:_0x346e9c,body:_0x587cc2,isCmd:_0x58ae5a,command:_0x4a241a,args:_0x308bfd,q:_0x2ee977,isGroup:_0x4aae72,sender:_0x4df5a6,senderNumber:_0x3081af,botNumber2:_0x4e3db8,botNumber:_0x3bc9e4,pushname:_0x20aabc,isMe:_0x347fb1,isOwner:_0x4f7e21,groupMetadata:_0x482c3e,groupName:_0x39c270,participants:_0x2b189b,groupAdmins:_0x56ac33,isBotAdmins:_0x2f429a,isAdmins:_0x2653fd,reply:_0x1d2525})=>{const _0x486044=_0x1539a9;try{const _0x3fd6ce=_0x308bfd[_0x486044(0x1c1)]('\x20');if(!_0x3fd6ce)return _0x1d2525(_0x486044(0x1d2));const _0x48385e=_0x486044(0x1d4)+_0x3fd6ce,_0x534f28=await axios[_0x486044(0x1d6)](_0x48385e),_0x1b3825=_0x534f28['data'];let _0x340a45=_0x486044(0x1dd);_0x340a45+=_0x486044(0x1d3)+_0x1b3825[_0x486044(0x1c3)]+'\x0a',_0x340a45+=_0x486044(0x1dc)+_0x1b3825[_0x486044(0x1d0)]+'\x0a',_0x340a45+=_0x486044(0x1c8)+_0x1b3825['description']+'\x0a',_0x340a45+='⭐\x20*ꜱᴛᴀʀꜱ*:\x20'+_0x1b3825[_0x486044(0x1ca)]+'\x0a',_0x340a45+=_0x486044(0x1c5)+_0x1b3825[_0x486044(0x1d8)]+'\x0a',_0x340a45+='\x0a',_0x340a45+=_0x486044(0x1c7),await _0x5dde0d[_0x486044(0x1da)](_0x207f7c,{'text':_0x340a45},{'quoted':_0xe0ba24});}catch(_0x1338b0){console[_0x486044(0x1df)](_0x1338b0),_0x1d2525(_0x486044(0x1e0)+_0x1338b0[_0x486044(0x1de)]);}});function _0x1f42(){const _0x18c7a8=['../command','join','Fetch\x20information\x20about\x20a\x20GitHub\x20repository.','name','60fTQHmM','🍴\x20*ꜰᴏʀᴋꜱ*:\x20','198104exMfUm','*RAHUL-MD\x20CREATION*\x0a','📝\x20*ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ*:\x20','3XtUnid','stargazers_count','182046vDkqXc','177912gjAMUC','axios','99pPTkZC','194865fnWXnr','html_url','127040AZUoji','Please\x20provide\x20a\x20GitHub\x20repository\x20name\x20in\x20the\x20format\x20📌`owner/repo`.','📌\x20*ɴᴀᴍᴇ*:\x20','https://api.github.com/repos/','3045308tKaENk','get','1VIlocs','forks_count','1419805zPxbuW','sendMessage','12knTMnp','🔗\x20*ᴜʀʟ*:\x20','📁_*GITHUB\x20REPOSITORY\x20INFO\x20BY\x20RAHUL-MD*_📁\x0a\x0a','message','log','Error\x20fetching\x20repository\x20data🤕:\x20'];_0x1f42=function(){return _0x18c7a8;};return _0x1f42();}
