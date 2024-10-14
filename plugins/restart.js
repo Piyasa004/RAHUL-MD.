a3bf80a321759a922f7c77aeb0777c2b1308bb68
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-15 00:08:53 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,23 +1,28 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const {sleep} = require('../lib/functions')
 
-cmd({
-    pattern: "restart",
-    react: "🐬",
-    desc: "restart the bot",
-    category: "owner",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if (!isOwner) return;
-const {exec} = require("child_process")
-reply("restarting...")
-await sleep(1500)
-exec("pm2 restart all")
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+function _0x49b7(){const _0x55781a=['6uHqgWA','log','restarting...','7966FoFDWT','restart\x20the\x20bot','restart','owner','29588voBRxr','12QUUgyC','18270802PfUJyl','../lib/functions','27Dzuyjn','3396808fpHzWx','../config','61drMXUE','3402948xHpxpl','../command','372FNQNNV','3137380hwrLlV','4947960brrrbr'];_0x49b7=function(){return _0x55781a;};return _0x49b7();}const _0x41058e=_0x2575;(function(_0x2989fb,_0x3a639c){const _0x127960=_0x2575,_0x4f8c01=_0x2989fb();while(!![]){try{const _0x2810ea=parseInt(_0x127960(0x1d4))/0x1*(-parseInt(_0x127960(0x1cd))/0x2)+parseInt(_0x127960(0x1da))/0x3*(parseInt(_0x127960(0x1d5))/0x4)+-parseInt(_0x127960(0x1d9))/0x5+-parseInt(_0x127960(0x1d7))/0x6*(parseInt(_0x127960(0x1dd))/0x7)+parseInt(_0x127960(0x1d2))/0x8+parseInt(_0x127960(0x1d1))/0x9*(-parseInt(_0x127960(0x1d8))/0xa)+parseInt(_0x127960(0x1cf))/0xb*(parseInt(_0x127960(0x1ce))/0xc);if(_0x2810ea===_0x3a639c)break;else _0x4f8c01['push'](_0x4f8c01['shift']());}catch(_0x263d64){_0x4f8c01['push'](_0x4f8c01['shift']());}}}(_0x49b7,0xd7a3d));function _0x2575(_0x79a6f0,_0x11ba58){const _0x49b7ff=_0x49b7();return _0x2575=function(_0x25750d,_0x18029f){_0x25750d=_0x25750d-0x1cd;let _0x998910=_0x49b7ff[_0x25750d];return _0x998910;},_0x2575(_0x79a6f0,_0x11ba58);}const config=require(_0x41058e(0x1d3)),{cmd,commands}=require(_0x41058e(0x1d6)),{sleep}=require(_0x41058e(0x1d0));cmd({'pattern':_0x41058e(0x1df),'react':'🐬','desc':_0x41058e(0x1de),'category':_0x41058e(0x1e0),'filename':__filename},async(_0x2d2836,_0x56be1f,_0xdb4437,{from:_0xf8481e,quoted:_0x2db4d1,body:_0x396580,isCmd:_0x190e85,command:_0x5d8f50,args:_0x6b65a4,q:_0x2119f6,isGroup:_0x6fbec2,sender:_0x35a0d8,senderNumber:_0x22d3c8,botNumber2:_0x9232b7,botNumber:_0x32ca69,pushname:_0x5c4579,isMe:_0x172579,isOwner:_0x48aaa9,groupMetadata:_0x3b5c2c,groupName:_0x4915ce,participants:_0x460445,groupAdmins:_0x376a5d,isBotAdmins:_0x13924c,isAdmins:_0x296cc1,reply:_0x309f9b})=>{const _0xd7e237=_0x41058e;try{if(!_0x48aaa9)return;const {exec:_0x3210a6}=require('child_process');_0x309f9b(_0xd7e237(0x1dc)),await sleep(0x5dc),_0x3210a6('pm2\x20restart\x20all');}catch(_0x203222){console[_0xd7e237(0x1db)](_0x203222),_0x309f9b(''+_0x203222);}});
