dc50b7da7fb90f62496d5a8b4823d1f29d57c694
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-14 22:20:14 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,25 +1,28 @@
-const config = require('../config')
-let fs = require('fs')
-const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
-cmd({
-    pattern: "ping",
-    react: "🤖",
-    alias: ["speed"],
-    desc: "Check bot\'s ping",
-    category: "main",
-    use: '.ping',
-    filename: __filename
-},
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '*_ZINDA HUU_*'  }, { quoted: mek } )
-var final = new Date().getTime();
-await conn.sendMessage(from, { delete: ping.key })
-return await conn.sendMessage(from , { text: '*🫶🏻 RAHUL MD PING*\n *' + (final - inital) + ' MS* '  }, { quoted: mek } )
-} catch (e) {
-reply('*Error !!*')
-l(e)
-}
-})
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+const _0x2c80bb=_0x11d1;(function(_0x1355e2,_0x3d56bd){const _0x3319a3=_0x11d1,_0x58ed87=_0x1355e2();while(!![]){try{const _0x2a92fe=parseInt(_0x3319a3(0x98))/0x1+-parseInt(_0x3319a3(0x97))/0x2+-parseInt(_0x3319a3(0x8f))/0x3*(-parseInt(_0x3319a3(0x88))/0x4)+parseInt(_0x3319a3(0x95))/0x5+parseInt(_0x3319a3(0x84))/0x6+parseInt(_0x3319a3(0x8c))/0x7*(-parseInt(_0x3319a3(0x90))/0x8)+parseInt(_0x3319a3(0x92))/0x9;if(_0x2a92fe===_0x3d56bd)break;else _0x58ed87['push'](_0x58ed87['shift']());}catch(_0x14f16c){_0x58ed87['push'](_0x58ed87['shift']());}}}(_0x2e8e,0x4d847));const config=require(_0x2c80bb(0x8d));let fs=require('fs');const {cmd,commands}=require(_0x2c80bb(0x83)),{getBuffer,getGroupAdmins,getRandom,h2k,isUrl,Json,runtime,sleep,fetchJson}=require('../lib/functions');function _0x11d1(_0x44537f,_0x1ac73f){const _0x2e8e77=_0x2e8e();return _0x11d1=function(_0x11d11a,_0x550902){_0x11d11a=_0x11d11a-0x83;let _0xf0bdc7=_0x2e8e77[_0x11d11a];return _0xf0bdc7;},_0x11d1(_0x44537f,_0x1ac73f);}cmd({'pattern':_0x2c80bb(0x8b),'react':'ðŸ¤–','alias':[_0x2c80bb(0x91)],'desc':_0x2c80bb(0x89),'category':_0x2c80bb(0x86),'use':_0x2c80bb(0x8e),'filename':__filename},async(_0x4ae6a4,_0x5569bb,_0x24fc9f,{from:_0x576535,l:_0xf72e3d,quoted:_0x4e6930,body:_0x4de190,isCmd:_0x4e7fbc,command:_0x2509ac,args:_0x2f2248,q:_0x2ba085,isGroup:_0x347d2d,sender:_0x5d1b1b,senderNumber:_0x5b4455,botNumber2:_0x57abcf,botNumber:_0x17ba11,pushname:_0x3b8c75,isMe:_0x28b668,isOwner:_0x316c8d,groupMetadata:_0xeebade,groupName:_0xa5a8c4,participants:_0x1fb786,groupAdmins:_0x50af0a,isBotAdmins:_0x58845f,isAdmins:_0x39395a,reply:_0x38acf3})=>{const _0x3f595f=_0x2c80bb;try{var _0x210226=new Date()[_0x3f595f(0x96)]();let _0xb2c676=await _0x4ae6a4[_0x3f595f(0x85)](_0x576535,{'text':_0x3f595f(0x93)},{'quoted':_0x5569bb});var _0x7c3fa5=new Date()[_0x3f595f(0x96)]();return await _0x4ae6a4['sendMessage'](_0x576535,{'delete':_0xb2c676[_0x3f595f(0x8a)]}),await _0x4ae6a4[_0x3f595f(0x85)](_0x576535,{'text':_0x3f595f(0x87)+(_0x7c3fa5-_0x210226)+_0x3f595f(0x94)},{'quoted':_0x5569bb});}catch(_0x163d22){_0x38acf3('*Error\x20!!*'),_0xf72e3d(_0x163d22);}});function _0x2e8e(){const _0xf952c7=['*_ZINDA\x20HUU_*','\x20MS*\x20','1402955LNWURC','getTime','968208ySLHRp','47317fqKtFt','../command','1203090QzPtQv','sendMessage','main','*ðŸ«¶ðŸ»\x20RAHUL\x20MD\x20PING*\x0a\x20*','2310180OUyAcO','Check\x20bot\x27s\x20ping','key','ping','84WaREia','../config','.ping','3dgKCbg','274496efotdg','speed','966519LvrkBv'];_0x2e8e=function(){return _0xf952c7;};return _0x2e8e();}
