e0721f756423a4d5e843b28b75e4c2fd09ef9fce
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-14 22:23:10 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,21 +1,28 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
 
-cmd({
-    pattern: "alive",
-    react: "🌐",
-    desc: "Check bot online or no.",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const config = await readEnv();
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
 
 
+
+const _0x54be34=_0x3df3;(function(_0x3f7a8e,_0x8aae25){const _0x42af07=_0x3df3,_0x2b5027=_0x3f7a8e();while(!![]){try{const _0x7d686f=parseInt(_0x42af07(0xf2))/0x1+-parseInt(_0x42af07(0xeb))/0x2+parseInt(_0x42af07(0xe8))/0x3*(-parseInt(_0x42af07(0xf6))/0x4)+parseInt(_0x42af07(0xf4))/0x5*(parseInt(_0x42af07(0xef))/0x6)+parseInt(_0x42af07(0xf0))/0x7*(parseInt(_0x42af07(0xea))/0x8)+parseInt(_0x42af07(0xf3))/0x9+-parseInt(_0x42af07(0xf1))/0xa*(parseInt(_0x42af07(0xe9))/0xb);if(_0x7d686f===_0x8aae25)break;else _0x2b5027['push'](_0x2b5027['shift']());}catch(_0xd6a533){_0x2b5027['push'](_0x2b5027['shift']());}}}(_0x2ba6,0x8b4d9));const {readEnv}=require(_0x54be34(0xec)),{cmd,commands}=require(_0x54be34(0xf5));function _0x3df3(_0x427715,_0x4d457e){const _0x2ba68a=_0x2ba6();return _0x3df3=function(_0x3df376,_0x5e7382){_0x3df376=_0x3df376-0xe6;let _0x4ad0d6=_0x2ba68a[_0x3df376];return _0x4ad0d6;},_0x3df3(_0x427715,_0x4d457e);}cmd({'pattern':_0x54be34(0xe6),'react':'ðŸŒ','desc':'Check\x20bot\x20online\x20or\x20no.','category':'main','filename':__filename},async(_0x127e62,_0x5d64fa,_0x5c753a,{from:_0x5db889,quoted:_0x1beebc,body:_0x7547e0,isCmd:_0x557740,command:_0x470e8e,args:_0x117327,q:_0x2f4fad,isGroup:_0x430203,sender:_0x375f89,senderNumber:_0x2fbdaa,botNumber2:_0x5483d3,botNumber:_0x23b0d3,pushname:_0x4cb2e5,isMe:_0x3aa30b,isOwner:_0x2540a7,groupMetadata:_0x2fdfc5,groupName:_0x398d11,participants:_0x252495,groupAdmins:_0x473837,isBotAdmins:_0x11e8f2,isAdmins:_0x287859,reply:_0x15728c})=>{const _0x3cc641=_0x54be34;try{const _0x31e125=await readEnv();return await _0x127e62[_0x3cc641(0xed)](_0x5db889,{'image':{'url':_0x31e125[_0x3cc641(0xe7)]},'caption':_0x31e125[_0x3cc641(0xee)]},{'quoted':_0x5d64fa});}catch(_0x32c9a5){console['log'](_0x32c9a5),_0x15728c(''+_0x32c9a5);}});function _0x2ba6(){const _0x2493eb=['433670otsENO','../lib/database','sendMessage','ALIVE_MSG','15192hXPqEB','1359757MlnWKA','480KEAGMl','690554VLOfhz','3157452ilHxKB','225ScwFWC','../command','4nzMSDu','alive','ALIVE_IMG','1016973JwkiLe','140173heEgAR','24HkORqx'];_0x2ba6=function(){return _0x2493eb;};return _0x2ba6();}
