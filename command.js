81a5d4d64e5c58b33a63bee02b9ab5d00c84db76
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-14 21:58:42 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- command.js
+++ command.js
@@ -1,20 +1,23 @@
-var commands = [];
 
-function cmd(info, func) {
-    var data = info;
-    data.function = func;
-    if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
-    if (!data.fromMe) data.fromMe = false;
-    if (!info.category) data.category = 'misc';
-    if(!info.filename) data.filename = "Not Provided";
-    commands.push(data);
-    return data;
-}
-module.exports = {
-    cmd,
-    AddCommand:cmd,
-    Function:cmd,
-    Module:cmd,
-    commands,
-};
+/**
+
+
+██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗ 
+██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗
+██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║
+██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║
+██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝ 
+
+
+* Project Name - RAHUL-MD
+* Author - Rahul Debnath
+* YouTube - Rahul Tech
+* Version - V1
+
+World best and powerfull whatsapp user bot in India*/
+
+
+
+
+var _0x4c248e=_0x3eca;(function(_0xd3356c,_0x3bcaf9){var _0x1037e7=_0x3eca,_0x3fe69d=_0xd3356c();while(!![]){try{var _0x4f4d6f=-parseInt(_0x1037e7(0xfe))/0x1*(-parseInt(_0x1037e7(0xfa))/0x2)+-parseInt(_0x1037e7(0xf8))/0x3*(parseInt(_0x1037e7(0xf7))/0x4)+parseInt(_0x1037e7(0xf5))/0x5+-parseInt(_0x1037e7(0xf2))/0x6*(parseInt(_0x1037e7(0x100))/0x7)+parseInt(_0x1037e7(0xf4))/0x8*(parseInt(_0x1037e7(0x101))/0x9)+-parseInt(_0x1037e7(0xfd))/0xa*(-parseInt(_0x1037e7(0xfc))/0xb)+-parseInt(_0x1037e7(0xf3))/0xc*(-parseInt(_0x1037e7(0xf6))/0xd);if(_0x4f4d6f===_0x3bcaf9)break;else _0x3fe69d['push'](_0x3fe69d['shift']());}catch(_0x464cc3){_0x3fe69d['push'](_0x3fe69d['shift']());}}}(_0x50ef,0x3c429));var commands=[];function _0x50ef(){var _0x452a32=['35916VgfuKD','8AQRcYU','203685LdEgQC','1469wWLxxh','67324rZdViv','6yRQOgc','dontAddCommandList','8ZmRVKK','category','121BPtRUJ','2120vuQuUr','26830xmZFzf','function','24143rCHmdy','1851921ZRRjmM','exports','fromMe','desc','filename','720OIDyMU'];_0x50ef=function(){return _0x452a32;};return _0x50ef();}function _0x3eca(_0x56d9f6,_0x7631d9){var _0x50ef20=_0x50ef();return _0x3eca=function(_0x3eca29,_0x1aec42){_0x3eca29=_0x3eca29-0xf2;var _0x262eca=_0x50ef20[_0x3eca29];return _0x262eca;},_0x3eca(_0x56d9f6,_0x7631d9);}function cmd(_0x353c3a,_0x15c256){var _0x5c38d2=_0x3eca,_0x50a302=_0x353c3a;_0x50a302[_0x5c38d2(0xff)]=_0x15c256;if(!_0x50a302[_0x5c38d2(0xf9)])_0x50a302[_0x5c38d2(0xf9)]=![];if(!_0x353c3a[_0x5c38d2(0x104)])_0x353c3a[_0x5c38d2(0x104)]='';if(!_0x50a302[_0x5c38d2(0x103)])_0x50a302[_0x5c38d2(0x103)]=![];if(!_0x353c3a[_0x5c38d2(0xfb)])_0x50a302[_0x5c38d2(0xfb)]='misc';if(!_0x353c3a[_0x5c38d2(0x105)])_0x50a302[_0x5c38d2(0x105)]='Not\x20Provided';return commands['push'](_0x50a302),_0x50a302;}module[_0x4c248e(0x102)]={'cmd':cmd,'AddCommand':cmd,'Function':cmd,'Module':cmd,'commands':commands};
