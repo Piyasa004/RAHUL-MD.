770ea537521e34f119eac0d99d1d4923ff60296a
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-16 00:06:27 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/system.js
+++ plugins/system.js
@@ -1,25 +1,28 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const os = require("os")
-const {runtime} = require('../lib/functions')
-cmd({
-    pattern: "system",
-    react: "♠️",
-    alias: ["status","botinfo"],
-    desc: "Check up time, ram usage and more",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Rag Usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*Host Name:* ${os.hostname()}
-*Owner:* Rahul Debnath
-`
-return reply(`${status}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})  
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+const _0xe55f43=_0x1a7a;function _0x1a7a(_0xf0f40,_0x3b3590){const _0xb27975=_0xb279();return _0x1a7a=function(_0x1a7a77,_0xf3d17d){_0x1a7a77=_0x1a7a77-0x142;let _0x42791f=_0xb27975[_0x1a7a77];return _0x42791f;},_0x1a7a(_0xf0f40,_0x3b3590);}(function(_0x140894,_0x10c5cd){const _0x103236=_0x1a7a,_0x190a2a=_0x140894();while(!![]){try{const _0x8612be=parseInt(_0x103236(0x156))/0x1+-parseInt(_0x103236(0x15b))/0x2*(parseInt(_0x103236(0x150))/0x3)+-parseInt(_0x103236(0x146))/0x4+parseInt(_0x103236(0x14a))/0x5+-parseInt(_0x103236(0x142))/0x6*(-parseInt(_0x103236(0x147))/0x7)+parseInt(_0x103236(0x149))/0x8*(parseInt(_0x103236(0x154))/0x9)+parseInt(_0x103236(0x14c))/0xa*(-parseInt(_0x103236(0x15a))/0xb);if(_0x8612be===_0x10c5cd)break;else _0x190a2a['push'](_0x190a2a['shift']());}catch(_0x4eb893){_0x190a2a['push'](_0x190a2a['shift']());}}}(_0xb279,0x798e2));function _0xb279(){const _0x523b04=['1758905frIlvK','\x0a*Rag\x20Usage:*\x20','10HPkYAy','botinfo','main','totalmem','591171zubJCm','\x0a*Owner:*\x20*Rahul\x20Debnath*\x0a','../config','heapUsed','18675bgVmzV','../command','727057heFTSK','uptime','log','MB\x0a*Host\x20Name:*\x20','20072371OpDzgR','2nzVSVQ','6yPAfnb','\x20MB\x20/\x20','status','system','594012oAeKjt','4415061XILyKK','memoryUsage','3696LkBcRo'];_0xb279=function(){return _0x523b04;};return _0xb279();}const config=require(_0xe55f43(0x152)),{cmd,commands}=require(_0xe55f43(0x155)),os=require('os'),{runtime}=require('../lib/functions');cmd({'pattern':_0xe55f43(0x145),'react':'♠️','alias':[_0xe55f43(0x144),_0xe55f43(0x14d)],'desc':'Check\x20up\x20time,\x20ram\x20usage\x20and\x20more','category':_0xe55f43(0x14e),'filename':__filename},async(_0x59d9be,_0x55273b,_0x5cfcca,{from:_0x30f746,quoted:_0x1f80f1,body:_0x5ca6d0,isCmd:_0xae852,command:_0x574fd4,args:_0x30643e,q:_0x5b5992,isGroup:_0x439051,sender:_0x51a0fe,senderNumber:_0x55794c,botNumber2:_0x261008,botNumber:_0xf76239,pushname:_0x240ce2,isMe:_0x12373e,isOwner:_0x1b9959,groupMetadata:_0x1778c7,groupName:_0x2c9f32,participants:_0x4d87f7,groupAdmins:_0x1e1628,isBotAdmins:_0x2f1474,isAdmins:_0x742146,reply:_0x1306fd})=>{const _0x1609e5=_0xe55f43;try{let _0x2c8484='*Uptime:*\x20\x20'+runtime(process[_0x1609e5(0x157)]())+_0x1609e5(0x14b)+(process[_0x1609e5(0x148)]()[_0x1609e5(0x153)]/0x400/0x400)['toFixed'](0x2)+_0x1609e5(0x143)+Math['round'](require('os')[_0x1609e5(0x14f)]/0x400/0x400)+_0x1609e5(0x159)+os['hostname']()+_0x1609e5(0x151);return _0x1306fd(''+_0x2c8484);}catch(_0x196895){console[_0x1609e5(0x158)](_0x196895),_0x1306fd(''+_0x196895);}});
